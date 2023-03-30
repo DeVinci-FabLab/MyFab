f786377b3aa4dd93e59ab9bd1ea2518c389222d7
CodyIsTheSenate <mathieuman78@gmail.com>
2023-03-30 19:01:49 +0200

    Remove db in test for user/rules

--- back/tests/api/user/rules.test.js
+++ back/tests/api/user/rules.test.js
@@ -1,27 +1,12 @@
-const executeQuery = require("../../../functions/dataBase/executeQuery").run;
-let db;
-let idRoleTest;
-let idRoleProtectedTest;
-
-beforeAll(async () => {
-  db = await require("../../../functions/dataBase/createConnection").open({ isTest: true });
-});
-
-afterAll(() => {
-  db.end();
-});
-
 describe("GET /api/user/validateRules/", () => {
   test("200", async () => {
-    const user = "rulesPut200";
-    const userData = await require("../../createNewUserAndLog").createNewUserAndLog(db, executeQuery, user);
-    expect(userData, "User '" + user + "' already exist").not.toBe(0);
-
+    //Execute
     const data = {
-      userId: userData,
+      userId: 1,
       app: {
-        db: db,
-        executeQuery: executeQuery,
+        executeQuery: async (db, query, options) => {
+          return [null, { affectedRows: 1 }];
+        },
       },
     };
     const response = await require("../../../api/user/rules").putValidateRules(data);
@@ -30,32 +15,22 @@ describe("GET /api/user/validateRules/", () => {
   });
 
   test("204userAlreadyValidated", async () => {
-    const user = "rulesPut204userAlreadyValidated";
-    const userData = await require("../../createNewUserAndLog").createNewUserAndLog(db, executeQuery, user);
-    expect(userData, "User '" + user + "' already exist").not.toBe(0);
-
     const data = {
-      userId: userData,
+      userId: 1,
       app: {
-        db: db,
-        executeQuery: executeQuery,
+        executeQuery: async (db, query, options) => {
+          return [null, { affectedRows: 0 }];
+        },
       },
     };
-    await require("../../../api/user/rules").putValidateRules(data);
     const response = await require("../../../api/user/rules").putValidateRules(data);
     expect(response.code).toBe(204);
     expect(response.type).toBe("code");
   });
-  test("401noUser", async () => {
-    const user = "rulesPut401noUser";
-    const userData = await require("../../createNewUserAndLog").createNewUserAndLog(db, executeQuery, user);
-    expect(userData, "User '" + user + "' already exist").not.toBe(0);
 
+  test("401noUser", async () => {
     const data = {
-      app: {
-        db: db,
-        executeQuery: executeQuery,
-      },
+      app: {},
     };
     const response = await require("../../../api/user/rules").putValidateRules(data);
     expect(response.code).toBe(401);
