f10776ce2b6eec4dec4d9aceae1529609ca68e44
CodySen <58334020+MathieuSchl@users.noreply.github.com>
2023-04-14 18:08:48 +0200

    Add cypress (#15)
    * Add workflow for cypress
    
    * Use  cypress-io
    
    * Update Tests-front.yml
    
    * Update Tests-front.yml
    
    * Update Tests-front.yml
    
    * Update Tests-front.yml
    
    * Update Tests-front.yml
    
    * Update Tests-front.yml
    
    * Fix video option for cypress
    
    * Update Tests-front.yml
    
    * Add console.log in order to test github action
    
    * Update Tests-front.yml
    
    * Update Tests-front.yml
    
    * Update Tests-front.yml
    
    * Update Tests-front.yml
    
    * Update Tests-front.yml
    
    * Remove console.log
    
    * Add sumarry to github action
    
    * Improve summarry
    
    * Fix file location
    
    * search coverage.txt
    
    * Add grep
    
    * change grep to step
    
    * Change directory for grep
    
    * Change grep to step
    
    * Save results into tmp/
    
    * Fix coverrage to unit-test
    
    * Move test pass outside of table
    
    * Add metrics to build tests
    
    * Add tables to builds with github actions
    
    * Fix build tables
    
    * Fix back image name for table github actions
    
    * Change output for test-units

---
+++ front/cypress/e2e/pages/403.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe("Page 403", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/" + Cypress.env().BASE_PATH + "/404", { failOnStatusCode: false });
+  });
+
+  it("Go back to auth", () => {
+    cy.log(Cypress.env().IS_TEST_MODE);
+    cy.get(".back-button").first().click();
+    cy.get(".login-button", {
+      timeout: 10000,
+    }).should("be.visible");
+    cy.location("href").should("eq", "http://localhost:3000/" + Cypress.env().BASE_PATH + "/auth");
+  });
+});
