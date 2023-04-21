2367034868d81743d36b0d3a2292f2ee10920ab4
l_eg <legcom6@gmail.com>
2023-04-21 20:51:17 +0900

    Added CronJob for daily restarts

--- agent/functions/cron.js
+++ agent/functions/cron.js
@@ -2,12 +2,25 @@ const axios = require("axios");
 const CronJob = require("cron").CronJob;
 require("dotenv").config();
 const env_name = process.env.ENV_NAME.trim();
+const restart_cron_time =
+  process.env.RESTART_CRON_TIME?.trim() || "0 12 2 * * *";
+const always_restart = process.env.ALWAYS_RESTART?.includes("true");
 const servicesManager = require("./service");
 const backLocation = "back/";
 
 module.exports.startCron = async (service) => {
-  const job = new CronJob("* * * * * *", function () {
-    console.log("You will see this message every second");
-  });
-  //job.start();
+  const dailyRestart = new CronJob(
+    restart_cron_time,
+    async function () {
+      console.log("Checking for updates...");
+      const behindHead = await servicesManager.gitPull();
+      if (behindHead || always_restart) {
+        service = await servicesManager.restartService(service);
+      } else {
+        console.log("No updates found");
+      }
+    },
+    null,
+    true
+  );
 };
