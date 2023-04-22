3df5b1445359cd260ee87ac79a3f0ca82bd04c5a
l_eg <legcom6@gmail.com>
2023-04-22 17:02:39 +0900

    Added failsafe CronJob

--- agent/functions/cron.js
+++ agent/functions/cron.js
@@ -2,11 +2,13 @@ const axios = require("axios");
 const CronJob = require("cron").CronJob;
 require("dotenv").config();
 const env_name = process.env.ENV_NAME.trim();
+const is_linux = !process.env.IS_LINUX?.includes("false");
 const restart_cron_time =
   process.env.RESTART_CRON_TIME?.trim() || "0 12 2 * * *";
 const always_restart = process.env.ALWAYS_RESTART?.includes("true");
+const failsafe_cron_time =
+  process.env.FAILSAFE_CRON_TIME?.trim() || "0 */15 9-23 * * *";
 const servicesManager = require("./service");
-const backLocation = "back/";
 
 module.exports.startCron = async (service) => {
   const dailyRestart = new CronJob(
@@ -23,4 +25,43 @@ module.exports.startCron = async (service) => {
     null,
     true
   );
+
+  const failsafe = new CronJob(
+    failsafe_cron_time,
+    async function () {
+      if (is_linux) url = "http://back:5000/api/ping";
+      else if (env_name == "back") url = "http://localhost:5000/api/ping";
+      else return;
+
+      console.log("Pinging '" + url + "'...");
+      ping(url)
+        .then(async (result) => {
+          if (!result) {
+            console.log("Ping failed");
+            saveService(service);
+          }
+        })
+        .catch((error) => {
+          console.error("Ping failed: ", error);
+          saveService(service);
+        });
+    },
+    null,
+    true
+  );
 };
+
+async function ping(url) {
+  try {
+    await axios.get(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+async function saveService(service) {
+  console.log("Attempting to save service...");
+  await servicesManager.gitPull();
+  service = await servicesManager.restartService(service);
+}
