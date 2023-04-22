5040acea6036084326825926f4aba68961a6d9c1
l_eg <legcom6@gmail.com>
2023-04-22 19:47:03 +0900

    Renamed 'ALWAYS_RESTART' env variable

--- agent/functions/cron.js
+++ agent/functions/cron.js
@@ -7,7 +7,7 @@ const env_name = process.env.ENV_NAME.trim();
 const is_linux = !process.env.IS_LINUX?.includes("false");
 const restart_cron_time =
   process.env.RESTART_CRON_TIME?.trim() || "0 12 2 * * *";
-const always_restart = process.env.ALWAYS_RESTART?.includes("true");
+const daily_pull_always_restarts = process.env.DAILY_PULL_ALWAYS_RESTARTS?.includes("true");
 const failsafe_cron_time =
   process.env.FAILSAFE_CRON_TIME?.trim() || "0 */15 9-23 * * *";
 const ping_url = is_linux ? "http://back:5000/api/ping" :
@@ -20,7 +20,7 @@ module.exports.startCron = async (service) => {
     async function () {
       console.log("Checking for updates...");
       const behindHead = await servicesManager.gitPull();
-      if (behindHead || always_restart) {
+      if (behindHead || daily_pull_always_restarts) {
         service = await servicesManager.restartService(service);
       } else {
         console.log("No updates found");
