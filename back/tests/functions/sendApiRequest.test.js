33c6194f7630ac8391347df70478693ede47bcdf
CodyIsTheSenate <mathieuman78@gmail.com>
2023-04-23 10:54:08 +0200

    Add coverage for sendApiRequest

---
+++ back/tests/functions/sendApiRequest.test.js
@@ -0,0 +1,19 @@
+describe("GET /api/ping/", () => {
+  test("service up", async () => {
+    const response =
+      await require("../../functions/sendApiRequest").sendApiRequest(
+        "https://github.com/"
+      );
+
+    expect(response).toBe(true);
+  });
+
+  test("service down", async () => {
+    const response =
+      await require("../../functions/sendApiRequest").sendApiRequest(
+        "ThisServiceIsDown"
+      );
+
+    expect(response).toBe(false);
+  });
+});
