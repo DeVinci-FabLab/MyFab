bfcd4f0597c0b1a2eaf4bc98816d9438a1989a47
CodyIsTheSenate <mathieuman78@gmail.com>
2023-05-01 11:15:19 +0200

    Add test for agent/env

---
+++ back/tests/api/agent/env.test.js
@@ -0,0 +1,91 @@
+describe("POST /api/sql/", () => {
+  test("200", async () => {
+    const data = {
+      app: {},
+      userAuthorization: {
+        checkSpecialCode: async () => {
+          return true;
+        },
+      },
+      backEnv: async () => {
+        return 200;
+      },
+      frontEnv: async () => {
+        return 200;
+      },
+    };
+
+    const response = await require("../../../api/agent/env").env(data);
+
+    expect(response.code).toBe(200);
+    expect(response.type).toBe("json");
+    expect(response.json).toStrictEqual({ back: 200, front: 200 });
+  });
+
+  test("404specialcodenotValid", async () => {
+    const data = {
+      app: {},
+      userAuthorization: {
+        checkSpecialCode: async () => {
+          return false;
+        },
+      },
+      backEnv: async () => {
+        return 200;
+      },
+      frontEnv: async () => {
+        return 200;
+      },
+    };
+
+    const response = await require("../../../api/agent/env").env(data);
+
+    expect(response.code).toBe(404);
+    expect(response.type).toBe("code");
+  });
+
+  test("404backNotResponding", async () => {
+    const data = {
+      app: {},
+      userAuthorization: {
+        checkSpecialCode: async () => {
+          return true;
+        },
+      },
+      backEnv: async () => {
+        return 404;
+      },
+      frontEnv: async () => {
+        return 200;
+      },
+    };
+
+    const response = await require("../../../api/agent/env").env(data);
+
+    expect(response.code).toBe(404);
+    expect(response.type).toBe("code");
+  });
+
+  test("200frontNotResponding", async () => {
+    const data = {
+      app: {},
+      userAuthorization: {
+        checkSpecialCode: async () => {
+          return true;
+        },
+      },
+      backEnv: async () => {
+        return 200;
+      },
+      frontEnv: async () => {
+        return 404;
+      },
+    };
+
+    const response = await require("../../../api/agent/env").env(data);
+
+    expect(response.code).toBe(200);
+    expect(response.type).toBe("json");
+    expect(response.json).toStrictEqual({ back: 200, front: 404 });
+  });
+});
