aac7f9938620d7498ba179f58f279de7e3ca8639
CodyIsTheSenate <mathieuman78@gmail.com>
2023-06-18 11:22:29 +0200

    Cron use name for change priority or status for tickets

--- back/functions/cron/updatePriority.js
+++ back/functions/cron/updatePriority.js
@@ -3,13 +3,13 @@ const CronJob = require("cron").CronJob;
 module.exports.action = action;
 async function action(app) {
   const queryOneWeek = `UPDATE printstickets
-                    SET i_priority = '2'
+                    SET i_priority = (SELECT i_id FROM gd_ticketpriority WHERE v_name = 'A traiter')
                     WHERE NOW() > DATE_ADD(dt_creationdate, INTERVAL 1 WEEK);`;
   const dbResOneWeek = await app.executeQuery(app.db, queryOneWeek, []);
   if (dbResOneWeek[0]) console.log(dbResOneWeek[0]);
 
   const queryTwoWeek = `UPDATE printstickets
-                    SET i_priority = '3'
+                    SET i_priority = (SELECT i_id FROM gd_ticketpriority WHERE v_name = 'Urgent')
                     WHERE NOW() > DATE_ADD(dt_creationdate, INTERVAL 2 WEEK);`;
   const dbResTwoWeek = await app.executeQuery(app.db, queryTwoWeek, []);
   if (dbResTwoWeek[0]) console.log(dbResTwoWeek[0]);
