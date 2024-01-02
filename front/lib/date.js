5cfeb568d8f1cba97b78a2a8b5755ef0ba9e460f
CodyIsTheSenate <mathieuman78@gmail.com>
2024-01-02 12:57:41 +0100

    Move dateDiff in lib

--- front/lib/date.js
+++ front/lib/date.js
@@ -16,3 +16,34 @@ export function format(date, style) {
       return moment(date).format("DD/MM/YYYY HH:mm");
   }
 }
+
+export function dateDiff(date1, date2) {
+  const diffInMilliseconds = Math.abs(date2 - date1);
+
+  const seconds = Math.floor(diffInMilliseconds / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(months / 12);
+
+  const remainingSeconds = seconds % 60;
+  const remainingMinutes = minutes % 60;
+  const remainingHours = hours % 24;
+  const remainingDays = days % 30;
+  const remainingMonths = months % 12;
+
+  if (years !== 0) return `${years} année${years > 1 ? "s" : ""}`;
+  if (remainingMonths !== 0) return `${remainingMonths} mois`;
+  if (remainingDays !== 0)
+    return `${remainingDays} jour${remainingDays > 1 ? "s" : ""}`;
+  if (remainingHours !== 0)
+    return `${remainingHours} heure${remainingHours > 1 ? "s" : ""}`;
+  if (remainingMinutes !== 0)
+    return `${remainingMinutes} minute${remainingMinutes > 1 ? "s" : ""}`;
+  if (remainingSeconds !== 0)
+    return `${remainingSeconds} seconde${remainingSeconds > 1 ? "s" : ""}`;
+  return `${diffInMilliseconds} milliseconde${
+    diffInMilliseconds > 1 ? "s" : ""
+  }`;
+}
