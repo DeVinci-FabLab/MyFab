09fcb9e1c94f29e294ec4708231ef0995bcabacf
CodyIsTheSenate <mathieuman78@gmail.com>
2024-01-25 17:08:53 +0100

    Run Linter

--- front/components/rules.js
+++ front/components/rules.js
@@ -2,25 +2,33 @@ export default function Rules() {
   return (
     <div>
       <p>
-        Toute inscription ou utilisation du site implique l'acceptation sans aucune réserve ni restriction des présentes
-        CGU par l’utilisateur. Lors de l'inscription sur le site via le Formulaire d’inscription, chaque utilisateur
-        accepte expressément les présentes CGU en cochant la case précédant le texte suivant : « Je reconnais avoir lu
-        et compris les CGU et je les accepte ».
+        Toute inscription ou utilisation du site implique l'acceptation sans
+        aucune réserve ni restriction des présentes CGU par l’utilisateur. Lors
+        de l'inscription sur le site via le Formulaire d’inscription, chaque
+        utilisateur accepte expressément les présentes CGU en cochant la case
+        précédant le texte suivant : « Je reconnais avoir lu et compris les CGU
+        et je les accepte ».
       </p>
       <p>
-        En cas de non-acceptation des CGU stipulées dans le présent contrat, l'Utilisateur se doit de renoncer à l'accès
-        des services proposés par le site.
+        En cas de non-acceptation des CGU stipulées dans le présent contrat,
+        l'Utilisateur se doit de renoncer à l'accès des services proposés par le
+        site.
       </p>
       <p>
-        Le DeVinci FabLab se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes CGU.
+        Le DeVinci FabLab se réserve le droit de modifier unilatéralement et à
+        tout moment le contenu des présentes CGU.
       </p>
       <h3>Les mentions légales</h3>
-      <p className="m-0">L'édition de ce site est assurée par l'association DeVinci FabLab régie par la loi de 1901.</p>
+      <p className="m-0">
+        L'édition de ce site est assurée par l'association DeVinci FabLab régie
+        par la loi de 1901.
+      </p>
       <p className="m-0">
         <strong>Numéro RNA : </strong>W922009573
       </p>
       <p className="m-0">
-        <strong>SIÈGE SOCIAL : </strong>12, avenue Léonard de Vinci, 92400 Courbevoie
+        <strong>SIÈGE SOCIAL : </strong>12, avenue Léonard de Vinci, 92400
+        Courbevoie
       </p>
       <p className="m-0">
         <strong>Adresse email : </strong>fablab@devinci.fr
@@ -34,8 +42,8 @@ export default function Rules() {
         </a>
       </p>
       <p>
-        L'hébergeur du site est la société OVH, dont le siège social est situé au 2 rue Kellermann, 59100 Roubaix,
-        France
+        L'hébergeur du site est la société OVH, dont le siège social est situé
+        au 2 rue Kellermann, 59100 Roubaix, France
       </p>
       <h3>Objectif</h3>
       <p>
@@ -43,15 +51,18 @@ export default function Rules() {
         <a className="text-blue-500" href="https://my.devinci-fablab.fr">
           https://my.devinci-fablab.fr
         </a>
-        , a comme objectif de fournir un service de demande d'impression 3d aux étudiants et au personnel des écoles
-        ESILV, EMLV et IIM. Ces impressions 3D sont réalisé par les membres de l'association.
+        , a comme objectif de fournir un service de demande d'impression 3d aux
+        étudiants et au personnel des écoles ESILV, EMLV et IIM. Ces impressions
+        3D sont réalisé par les membres de l'association.
       </p>
       <h3>Qui peut utiliser le service ?</h3>
       <p>
-        Pour utiliser le service, l'utilisateur doit être étudiant ou membre du personnel d'une des écoles ESILV, EMLV
-        ou IIM. L'utilisateur peut également être membre de l'association du DeVinci FabLab. Si un utilisateur ne
-        respect pas l'un de ces critères, l'association du DeVinci FabLab se réserve le droit de bannir cet utilisateur
-        sans préavis.
+        Pour utiliser le service, l'utilisateur doit être étudiant ou membre du
+        personnel d'une des écoles ESILV, EMLV ou IIM. L'utilisateur peut
+        également être membre de l'association du DeVinci FabLab. Si un
+        utilisateur ne respect pas l'un de ces critères, l'association du
+        DeVinci FabLab se réserve le droit de bannir cet utilisateur sans
+        préavis.
       </p>
       <h3>Données personnelles</h3>
       <p className="mb-0">
@@ -59,7 +70,8 @@ export default function Rules() {
         <a className="text-blue-500" href="https://my.devinci-fablab.fr">
           https://my.devinci-fablab.fr
         </a>{" "}
-        enregistre des données pour le fonctionnement du site. Elles sont de plusieurs type.
+        enregistre des données pour le fonctionnement du site. Elles sont de
+        plusieurs type.
       </p>
       <h4 className="mt-2 mb-0">Données utilisateurs</h4>
       <ul className="m-0">
@@ -82,51 +94,65 @@ export default function Rules() {
         <li className="m-0">Système d'exploitation</li>
       </ul>
       <p>
-        Il est possible de demander l'anonymisation ou la modification des données de votre compte utilisateur par mail
-        : fablab@devinci.fr.
+        Il est possible de demander l'anonymisation ou la modification des
+        données de votre compte utilisateur par mail : fablab@devinci.fr.
       </p>
       <h3>Votre contenu et votre conduite</h3>
       <p>
-        Le contenu que vous soumettez ne doit en aucun cas contenir la propriété intellectuelle d'un tiers, tels que des
-        éléments protégés par le droit d'auteur, à moins que vous n'ayez obtenu une autorisation expresse de ce tiers ou
-        que vous bénéficiez d'une autre autorisation légale conforme aux exceptions ou limitations prévues par le droit
-        de l'Union européenne en matière de droit d'auteur ou de droits voisins. De plus, l'utilisation du service pour
-        diffuser des contenus illégaux est strictement interdite, notamment ceux qui:
+        Le contenu que vous soumettez ne doit en aucun cas contenir la propriété
+        intellectuelle d'un tiers, tels que des éléments protégés par le droit
+        d'auteur, à moins que vous n'ayez obtenu une autorisation expresse de ce
+        tiers ou que vous bénéficiez d'une autre autorisation légale conforme
+        aux exceptions ou limitations prévues par le droit de l'Union européenne
+        en matière de droit d'auteur ou de droits voisins. De plus,
+        l'utilisation du service pour diffuser des contenus illégaux est
+        strictement interdite, notamment ceux qui:
       </p>
       <ul className="m-0">
         <li className="m-0">
-          Incitent à la haine, à la violence ou à la discrimination en raison de l'origine des personnes, de leur race,
-          de leur religion, de leur genre, de leur orientation sexuelle.
+          Incitent à la haine, à la violence ou à la discrimination en raison de
+          l'origine des personnes, de leur race, de leur religion, de leur
+          genre, de leur orientation sexuelle.
         </li>
         <li className="m-0">Sont constitutifs de harcèlement sexuel.</li>
         <li className="m-0">
-          Portent atteinte à la dignité humaine, tels que la traite des êtres humains ou le proxénétisme.
+          Portent atteinte à la dignité humaine, tels que la traite des êtres
+          humains ou le proxénétisme.
         </li>
-        <li className="m-0">Font l'apologie ou nient l'existence de crimes contre l'humanité.</li>
+        <li className="m-0">
+          Font l'apologie ou nient l'existence de crimes contre l'humanité.
+        </li>
         <li className="m-0">Font la promotion du terrorisme.</li>
         <li className="m-0">
-          Font l'apologie de crimes très graves contre les personnes, tels que le meurtre et les agressions sexuelles.
+          Font l'apologie de crimes très graves contre les personnes, tels que
+          le meurtre et les agressions sexuelles.
         </li>
         <li className="m-0">
-          Font l'apologie du vol, de l'extorsion et des atteintes aux biens, mettant ainsi en danger les personnes.
+          Font l'apologie du vol, de l'extorsion et des atteintes aux biens,
+          mettant ainsi en danger les personnes.
         </li>
         <li className="m-0">Sont constitutifs de pédopornographie.</li>
       </ul>
       <p>
-        Vous êtes légalement responsable des Contenus que vous soumettez au service. Lorsqu’un Contenu est identifié par
-        nos algorithmes comme étant illégal, il peut être supprimé en tout ou partie et votre accès à nos services
-        pourrait être suspendu ou bloqué. Pour en savoir plus, consultez la section "Suspension et clôture de compte"
-        ci-dessous.{" "}
+        Vous êtes légalement responsable des Contenus que vous soumettez au
+        service. Lorsqu’un Contenu est identifié par nos algorithmes comme étant
+        illégal, il peut être supprimé en tout ou partie et votre accès à nos
+        services pourrait être suspendu ou bloqué. Pour en savoir plus,
+        consultez la section "Suspension et clôture de compte" ci-dessous.{" "}
       </p>
       <h3>Limitation de responsabilité</h3>
       <p>
-        Le DeVinci FabLab décline toute responsabilité quant à l'exactitude, la fiabilité, l'exhaustivité ou la
-        pertinence des informations fournies sur ce site. Bien que le DeVinci FabLab s'efforce de maintenir le contenu à
-        jour, il ne garantit pas l'actualité des informations. Les utilisateurs reconnaissent et acceptent expressément
-        que l'utilisation de ce site se fait à leurs propres risques. En aucun cas, le DeVinci FabLab, le bureau, ou
-        membre de l'association ne pourra être tenu responsable des dommages directs, indirects, spéciaux, consécutifs
-        ou exemplaires résultant de l'utilisation ou de l'incapacité à utiliser le site, même si le DeVinci FabLab a été
-        informé de la possibilité de tels dommages. Cette limitation de responsabilité s'applique dans toute la mesure
+        Le DeVinci FabLab décline toute responsabilité quant à l'exactitude, la
+        fiabilité, l'exhaustivité ou la pertinence des informations fournies sur
+        ce site. Bien que le DeVinci FabLab s'efforce de maintenir le contenu à
+        jour, il ne garantit pas l'actualité des informations. Les utilisateurs
+        reconnaissent et acceptent expressément que l'utilisation de ce site se
+        fait à leurs propres risques. En aucun cas, le DeVinci FabLab, le
+        bureau, ou membre de l'association ne pourra être tenu responsable des
+        dommages directs, indirects, spéciaux, consécutifs ou exemplaires
+        résultant de l'utilisation ou de l'incapacité à utiliser le site, même
+        si le DeVinci FabLab a été informé de la possibilité de tels dommages.
+        Cette limitation de responsabilité s'applique dans toute la mesure
         permise par la loi applicable.
       </p>
     </div>
