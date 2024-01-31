426f3f047a3994ce2f9b46b25b6ed9149394e6a2
CodyIsTheSenate <mathieuman78@gmail.com>
2024-01-31 16:25:50 +0100

    Remove /panel/tokenView and test

--- front/pages/test.js
+++
@@ -1,9 +0,0 @@
-import { getCookie } from "cookies-next";
-import { UserUse } from "../context/provider";
-
-export default function Rules({ userNeedToAccept }) {
-  const jwt = getCookie("jwt");
-  const { user, setUser, contextError } = UserUse(jwt);
-
-  return <div>{user.firstName}</div>;
-}
