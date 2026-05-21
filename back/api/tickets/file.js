e7aeebca5bea3b89d2b5631963b600499eeec9d0
cyprienbqt <cyprien.buquet@icloud.com>
2026-05-21 20:14:28 +0200

    update file.js

--- back/api/tickets/file.js
+++ back/api/tickets/file.js
@@ -566,7 +566,8 @@ async function ticketFilePost(data) {
   //loop all files
   for (const file of files) {
     const fileNameSplited = file.name.split(".");
-    if (fileNameSplited[fileNameSplited.length - 1].toLowerCase() === "stl") {
+    const fileExt = fileNameSplited[fileNameSplited.lentgh -1].toLowerCase();
+    if (fileExt === "stl" || fileExt === "dxf") {
       const res = await new Promise(async (resolve) => {
         const newFileName = makeid(10, file.name);
         fs.copyFile(
